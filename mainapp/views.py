ae662e40600047bad71b690f12c1e8099da1ce58
Bholm620 <BHOLM886@GMAIL.COM>
2025-08-09 22:23:30 -0600

    sd

--- mainapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home(request):
-    products = ["Cherry", "Ull", "Oraiste", "Su Talon" ,"pears", "Wassermelon"]
-    context = {
-        'products': products,
-    }
-    return render(request,"home.html", context)
